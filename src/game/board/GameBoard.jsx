ed3347bd5e6531de71216b2ecf0c4561411d9ae2
ktpease <61762655+ktpease@users.noreply.github.com>
2023-05-17 06:50:16 -0400

    Rework dynamic tile sizing
    Tile "font" sizes are now dynamically generated based on the board size and scale better to fit the screen using the CSS min function.

--- src/game/board/GameBoard.jsx
+++ src/game/board/GameBoard.jsx
@@ -89,22 +89,77 @@ export default function GameBoard({
     ));
   };
 
+  const [horizontalTileStyle, setHorizontalTileStyle] = useState({});
+  const [verticalTileStyle, setVerticalTileStyle] = useState({});
+
+  useEffect(() => {
+    setHorizontalTileStyle({
+      fontSize:
+        "min(" +
+        (useEmoji
+          ? emojiFontSizeWidths[Math.min(Math.max(boardWidth, 2), 20) - 2]
+          : glyphFontSizeWidths[Math.min(Math.max(boardWidth, 2), 20) - 2]) +
+        "vw, " +
+        (useEmoji
+          ? emojiFontSizeHeights[Math.min(Math.max(boardHeight, 2), 12) - 2]
+          : glyphFontSizeHeights[Math.min(Math.max(boardHeight, 2), 12) - 2]) +
+        "vh)",
+    });
+    setVerticalTileStyle({
+      fontSize:
+        "min(" +
+        (useEmoji
+          ? emojiFontSizeHeights[Math.min(Math.max(boardHeight, 2), 12) - 2]
+          : glyphFontSizeHeights[Math.min(Math.max(boardHeight, 2), 12) - 2]) +
+        "vw, " +
+        (useEmoji
+          ? emojiFontSizeWidths[Math.min(Math.max(boardWidth, 2), 20) - 2]
+          : glyphFontSizeWidths[Math.min(Math.max(boardWidth, 2), 20) - 2]) +
+        "vh)",
+    });
+  }, [tiles, useEmoji]);
+
   return (
     <div>
       <div
-        className={`game-board game-board-horizontal game-board-size-${boardHeight} ${
+        className={`game-board game-board-horizontal ${
           useEmoji ? "game-board-emoji" : "game-board-glyph"
         }`}
+        style={horizontalTileStyle}
       >
         {renderTileMap(horizontalTileMap, "board-hori")}
       </div>
       <div
-        className={`game-board game-board-vertical game-board-size-${boardHeight} ${
+        className={`game-board game-board-vertical ${
           useEmoji ? "game-board-emoji" : "game-board-glyph"
         }`}
+        style={verticalTileStyle}
       >
         {renderTileMap(verticalTileMap, "board-vert")}
       </div>
     </div>
   );
 }
+
+// Magic numbers for use in font size calculations.
+// These were obtained through browser testing (changed until scrollbar became
+// visible in Chrome). These are very loose approximations and only apply to a
+// particular font and browser. TODO: Better way of doing this.
+
+const emojiFontSizeWidths = [
+  20.7, 20.7, 17.8, 15.3, 13.4, 11.9, 10.7, 9.7, 8.9, 8.2, 7.6, 7.1, 6.7, 6.3,
+  5.9, 5.6, 5.3, 5.1, 4.8,
+];
+const emojiFontSizeHeights = [
+  20.3, 16.3, 13.5, 11.7, 10.3, 9.1, 8.2, 7.5, 6.8, 6.3, 5.9, 5.5, 5.1, 4.8,
+  4.6, 4.3, 4.1, 3.9, 3.7,
+];
+
+const glyphFontSizeWidths = [
+  26.3, 25.5, 21.3, 18.2, 15.9, 14.2, 12.7, 11.6, 10.6, 9.8, 9.1, 8.5, 7.9, 7.5,
+  7.1, 6.7, 6.3, 6.0, 5.8,
+];
+const glyphFontSizeHeights = [
+  23.9, 19.5, 16.4, 14.2, 12.5, 11.2, 10.1, 9.2, 8.5, 7.8, 7.3, 6.8, 6.4, 6.0,
+  5.7, 5.4, 5.1, 4.9, 4.7,
+];
